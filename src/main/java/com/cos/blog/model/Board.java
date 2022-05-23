fc6082e81884fbb47d81fb96f933db5536b24e6d
tenta <eunoia3802@gmail.com>
2022-05-23 21:49:36 +0900

    Table 의존관계 설정 완료

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -1,15 +1,18 @@
 package com.cos.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
@@ -37,11 +40,14 @@ public class Board {
 	private String content; //섬머노트 라이브러리<html>태그가 섞여서 디자인 됨
 	
 	@ColumnDefault("0")
-	private int count;
+	private int count; //조회수
 	
-	@ManyToOne //Many = Board, User = One
+	@ManyToOne (fetch=FetchType.EAGER)//Many = Board, User = One
 	@JoinColumn(name="userId")
 	private User user; //DB는 오브젝트를 지정할 수 없다. 
+	
+	@OneToMany(mappedBy="board",fetch=FetchType.EAGER) // mappedBy 연관관계의 주인이 아니다 (난 FK가 아니에요) DB에 칼럼을 만들지 마세요.
+	private List<Reply> reply; //단지 reply 정보를 얻기위해..
 	
 	@CreationTimestamp
 	private Timestamp createDate;
