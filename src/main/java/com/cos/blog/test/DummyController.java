d76023ba8c170f032cccc59792b1139a975c3f2e
tenta <eunoia3802@gmail.com>
2022-05-26 22:22:20 +0900

    delete 설정 완료

--- src/main/java/com/cos/blog/test/DummyController.java
+++ src/main/java/com/cos/blog/test/DummyController.java
@@ -6,10 +6,12 @@ import java.util.function.Supplier;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +35,16 @@ public class DummyController {
 	//save함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 한다.
 	//email, password
 	
+	@DeleteMapping("/dummy/user/{id}")
+	public String delete(@PathVariable int id) {
+		try {
+		userRepository.deleteById(id);
+	}catch(EmptyResultDataAccessException e) {
+		return "삭제에 실패하였습니다. 해당 id는 DB에 없습니다.";
+	}
+	return "삭제되었습니다.id:"+id;
+	}
+
 	@Transactional
 	@PutMapping("/dummy/user/{id}")
 	public User updateUser(@PathVariable int id, @RequestBody User requestUser) {//json 데이터를 요청=>java object(MessageConverter의 Jackson 라이브러리가 변환해서 받아준다.)
@@ -49,7 +61,7 @@ public class DummyController {
 		// userRepository.save(user);
 		
 		//더티 채킹
-		return null;
+		return user;
 	}
 	
 	//http://localhost:8000/blog/dummy/user
