472206aab12bd271d2dd2b443ce9cdd66ae7d6c1
tenta <eunoia3802@gmail.com>
2022-07-26 18:16:36 +0900

    로그인 기능 설정(기본적인 스프링 방식 활용)

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,5 +1,7 @@
 package com.cos.blog.controller.api;
 
+import javax.servlet.http.HttpSession; 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,5 +26,16 @@ public class UserApiController {
 		user.setRole(RoleType.USER);
 		userService.회원가입(user);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(),1); //자바오브젝트를 JSON으로 변환해서 리턴(Jackson)    
+	}
+	
+	@PostMapping("/api/user/login")
+	public ResponseDto<Integer> login(@RequestBody User user, HttpSession session) {
+		System.out.println("UserApiController:login 호출됨");
+		User principal = userService.로그인(user); //principal (접근주체)
+		
+		if(principal != null) {
+			session.setAttribute("principal", principal);
+		}
+		return new ResponseDto<Integer>(HttpStatus.OK.value(),1);    
 	}
 }
