2b698217fde60ed6699a46a358bf545248672c7f
tenta <eunoia3802@gmail.com>
2022-07-28 18:33:42 +0900

    스프링 시큐리티를 이용한 로그인 기능 설정

--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -1,7 +1,8 @@
 package com.cos.blog.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.cos.blog.model.User;
 
@@ -9,7 +10,8 @@ import com.cos.blog.model.User;
 //자동으로 bean등록이 된다
 //@Repository 생량이 가능하다.
 public interface UserRepository extends JpaRepository<User,Integer>{
-
+	//SELECT * FROM user WHERE username = 1?;
+	Optional<User> findByUsername(String username);
 }
 
 
