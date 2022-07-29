01df3c16ad9fa0344a72d1566701ce621c1e9427
tenta <eunoia3802@gmail.com>
2022-07-29 16:18:13 +0900

    글쓰기 기능 추가

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -10,9 +10,14 @@ import com.cos.blog.config.auth.PrincipalDetail;
 public class BoardController {
 
 	@GetMapping({"","/"})
-	public String index(@AuthenticationPrincipal PrincipalDetail principal) {
+	public String index() {
 		// /WEB-INF/views/index.jsp
-		System.out.println("로그인 사용자 아이디:"+principal.getUsername());
 		return "index";
+	}
+	
+	//USER 권한이 필요
+	@GetMapping("/board/saveForm")
+	public String saveForm() {
+		return "board/saveForm";
 	}
 }
