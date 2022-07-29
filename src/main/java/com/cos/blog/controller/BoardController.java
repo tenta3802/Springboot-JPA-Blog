7a064562d1393782b84fc86b374a306d4f10e8d2
tenta <eunoia3802@gmail.com>
2022-07-29 17:14:57 +0900

    글쓰기 목록 기능 추가

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,6 +1,9 @@
 package com.cos.blog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +17,8 @@ public class BoardController {
 	private BoardService boardService;
 	
 	@GetMapping({"","/"})
-	public String index(Model model) {
-		model.addAttribute("boards",boardService.글목록());
+	public String index(Model model, @PageableDefault(size=3,sort="id",direction=Sort.Direction.DESC) Pageable pageable) {
+		model.addAttribute("boards",boardService.글목록(pageable));
 		return "index"; //Controller는 RestController와 다르게 viewResolver 작동!
 	}
 	
