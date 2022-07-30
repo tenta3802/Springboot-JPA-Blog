7dc65d34c588b3fa239a33a1daf02aff7fc5450e
tenta <eunoia3802@gmail.com>
2022-07-30 17:03:08 +0900

    글 삭제기능 추가

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -3,6 +3,10 @@ let index = {
 		$("#btn-save").on("click", () => { //function(){},()=>{} this를 바인딩하기 위해서!
 			this.save();
 		});
+
+		$("#btn-delete").on("click", () => { //function(){},()=>{} this를 바인딩하기 위해서!
+			this.deleteById();
+		});
 	},
 
 	save: function() {
@@ -10,21 +14,35 @@ let index = {
 			title: $("#title").val(),
 			content: $("#content").val(),
 		};
-		
+
 		$.ajax({
-			type:"POST",
-			url:"/api/board",
-			data:JSON.stringify(data), 
-			contentType:"application/json;charset=utf-8",
-			dataType:"json"
-		}).done(function(resp){
+			type: "POST",
+			url: "/api/board",
+			data: JSON.stringify(data),
+			contentType: "application/json;charset=utf-8",
+			dataType: "json"
+		}).done(function(resp) {
 			alert("글쓰기가 완료되었습니다.");
-			location.href="/";
-		}).fail(function(error){
+			location.href = "/";
+		}).fail(function(error) {
 			alert(JSON.stringify(error));
-		}); 
+		});
 	},
 
+	deleteById: function() {
+		var id = $("#id").text();
+
+		$.ajax({
+			type: "DELETE",
+			url: "/api/board/"+id,
+			dataType: "json"
+		}).done(function(resp) {
+			alert("삭제가 완료되었습니다.");
+			location.href = "/";
+		}).fail(function(error) {
+			alert(JSON.stringify(error));
+		});
+	},
 }
 
 
