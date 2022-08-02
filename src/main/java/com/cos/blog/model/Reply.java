4245dc7b8d70e76833579ca99e70cf1661fd2d35
tenta <eunoia3802@gmail.com>
2022-08-02 22:55:41 +0900

    댓글 작성 : 네이티브 쿼리 사용 (수정) & toString Test 완료

--- src/main/java/com/cos/blog/model/Reply.java
+++ src/main/java/com/cos/blog/model/Reply.java
@@ -41,6 +41,10 @@ public class Reply {
 	@CreationTimestamp
 	private Timestamp createDate;
 	
+	@Override
+	public String toString() {
+		return "Reply [id="+id+", content="+content+", board="+board+", user"+user+", createDate="+createDate+"]";
+	}
 }
 
 
